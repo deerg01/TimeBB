26b21f1de9c66b0f28bf17e7dea5fe3dcdc85aef
오수아 <2020048868@hanyang.ac.kr>
2023-08-12 21:41:40 +0900

    Initial Commit

---
+++ TimeBB/TimeBBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeBBApp.swift
+//  TimeBB
+//
+//  Created by xa on 2023/08/12.
+//
+
+import SwiftUI
+
+@main
+struct TimeBBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
